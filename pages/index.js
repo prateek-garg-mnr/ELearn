e1ec09e213dc64cc150f974a36a2dcc8b5856dc2
prateek-garg-mnr <prateek.garg@mckinleyrice.co>
2021-06-27 15:49:42 +0530

    remove <p>

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import React from 'react'
 function index() {
     return (
         <>
-            <h1 className="jumbotron text-center bg-primary">Hello world</h1>
-            <p>hello</p>
+            <h1 className="jumbotron text-center bg-primary">Online Education Marketplace</h1>
         </>
     )
 }
---
+++ pages/register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+    return (
+        <>
+        <h1 className="jumbotron bg-primary text-center square">Register</h1> 
+       </>
+    )
+}
+
+export default Register
