90db232be5593a9d3f00de8e016db0de26135aa6
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2021-06-27 20:05:12 +0530

    add custom style

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import TopNav from "../components/TopNav";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "antd/dist/antd.css";
+import '../public/css/style.css'
 
 function MyApp({ Component, pageProps }) {
   return (
