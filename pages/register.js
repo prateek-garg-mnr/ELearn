6159262e7e39348c55274130f7fc3d993bfdc457
prateek-garg-mnr <prateek.garg@mckinleyrice.co>
2021-06-27 16:03:23 +0530

    create register page

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Register() {
-    return (
-        <>
-        <h1 className="jumbotron bg-primary text-center square">Register</h1> 
-       </>
-    )
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ email, password, name });
+  };
+
+  return (
+    <>
+      <h1 className="jumbotron bg-primary text-center square">Register</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-4"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+          />
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter name"
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter name"
+          />
+          <button className="btn btn-block p-2 btn-primary" type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
