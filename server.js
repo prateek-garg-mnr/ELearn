5bfb9a7712b77d8dd86d26875a15b9923c050a0e
prateek-garg-mnr <prateek.garg@mckinleyrice.co>
2021-06-30 02:34:27 +0530

    proxy server for development

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const next = require("next");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    // apply proxy in dev mode
+    if (dev) {
+      server.use(
+        "/api",
+        createProxyMiddleware({
+          target: "http://localhost:8000",
+          changeOrigin: true,
+        })
+      );
+    }
+
+    server.all("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log("ready with next proxy server");
+    });
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
